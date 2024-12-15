009d818a531ca80da39f65b74a1f7fcc0a7ae21b
BugHunter-X <askzenai@gmail.com>
2024-12-15 04:48:05 +0100

    Add bug.groovy file

---
+++ bug.groovy
@@ -0,0 +1,13 @@
+```groovy
+def myMethod(a, b) {
+  if (a > b) {
+    return a
+  } else {
+    return b
+  }
+}
+
+println myMethod(5, 10) // Output: 10
+println myMethod(10, 5) // Output: 10
+println myMethod(5, 5) // Output: 5
+```
